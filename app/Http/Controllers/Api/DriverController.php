7c09a25cd1d32a98ba5eee400d00b3dd27ab4953
AhmedElabrass2030 <ahmeda.internal@metasoftware.eg>
2026-07-15 14:44:55 +0200

    feat : make DriverController with all CRUD functions and make validation in seperated requests (StoreDriverRequest,UpdateDriverRequest) and build DriverResource to control with the response

--- app/Http/Controllers/Api/DriverController.php
+++ app/Http/Controllers/Api/DriverController.php
@@ -3,9 +3,62 @@
 namespace App\Http\Controllers\Api;
 
 use App\Http\Controllers\Controller;
+use App\Http\Requests\Driver\StoreDriverRequest;
+use App\Http\Requests\Driver\UpdateDriverRequest;
+use App\Http\Resources\DriverResource;
+use App\Models\User;
+use App\Traits\ApiResponse;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Hash;
 
 class DriverController extends Controller
 {
-    //
+    use ApiResponse;
+    public function index()
+    {
+        $drivers = User::where('role', 'driver')->get();
+        return $this->success(DriverResource::collection($drivers), 'Drivers fetched successfully', 200);
+    }
+
+    public function store(StoreDriverRequest $request)
+    {
+        $data = $request->validated();
+        $user = User::create([
+            'name' => $data['name'],
+            'email' => $data['email'],
+            'password' => Hash::make($data['password']),
+            'phone' => $data['phone'] ?? null,
+            'role' => 'driver',
+            'is_active' => $data['is_active'],
+        ]);
+        return $this->success(
+            new DriverResource($user),
+            'Driver created successfully',
+            201
+        );
+    }
+    public function show(string $id)
+    {
+        $driver = User::where('role', 'driver')->findOrFail($id);
+        return $this->success(new DriverResource($driver), 'Driver fetched successfully', 200);
+    }
+
+    public function update(UpdateDriverRequest  $request, string $id)
+    {
+        $driver = User::where('role', 'driver')->findOrFail($id);
+        $data = $request->validated();
+        if (isset($data['password'])) {
+            $data['password'] = Hash::make($data['password']);
+        }
+        $driver->update($data);
+        return $this->success(new DriverResource($driver), 'Driver updated successfully', 200);
+    }
+
+
+    public function destroy(string $id)
+    {
+        $driver = User::where('role', 'driver')->findOrFail($id);
+        $driver->delete();
+        return $this->success(null, 'Driver deleted successfully', 200);
+    }
 }
--- routes/api.php
+++ routes/api.php
@@ -1,6 +1,7 @@
 <?php
 
 use App\Http\Controllers\Api\AuthController;
+use App\Http\Controllers\Api\DriverController;
 use Illuminate\Support\Facades\Route;
 
 
@@ -11,10 +12,13 @@ Route::prefix('auth')->group(function () {
     Route::middleware('auth:sanctum')->group(function () {
 
         Route::post('/logout', [AuthController::class,  'logout']);
-        
-        Route::get('/me', [AuthController::class,  'me']);
 
+        Route::get('/me', [AuthController::class,  'me']);
     });
 });
+// Route::apiResource('drivers', DriverController::class)->only(['index', 'show']);
+// Route::middleware('auth:sanctum')->group(function () {
+// Route::apiResource('drivers', DriverController::class)->except(['index', 'show']);
+// });
 
-
+Route::apiResource('drivers', DriverController::class);
