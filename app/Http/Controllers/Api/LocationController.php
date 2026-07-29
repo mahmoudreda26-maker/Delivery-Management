cf1d1479bea077143dd46efc64f513dc081d11ba
MariamEhabShaaban <ehabm0817@gmail.com>
2026-07-29 03:44:12 +0300

    overwrite with latest location event updates

--- app/Http/Controllers/Api/LocationController.php
+++ app/Http/Controllers/Api/LocationController.php
@@ -2,6 +2,15 @@
 
 namespace App\Http\Controllers\Api;
 
+<<<<<<< Updated upstream
+=======
+use App\Events\LocationUpdated;
+use App\Http\Controllers\Controller;
+use App\Http\Requests\LocationRequest;
+use App\Http\Resources\LocationResource;
+use App\Models\Vehicle;
+use App\Services\LocationService;
+>>>>>>> Stashed changes
 use App\Traits\ApiResponse;
 use App\Http\Controllers\Controller;
 use App\Http\Requests\StoreLocationRequest;
@@ -13,6 +22,7 @@ use Illuminate\Support\Facades\Auth;
 class LocationController extends Controller
 {
     use ApiResponse;
+<<<<<<< Updated upstream
    
     public function __construct(protected TrackingService $trackingService) {}
 
@@ -28,4 +38,22 @@ class LocationController extends Controller
             200
         );
     }
+=======
+    public function store(LocationRequest $request , LocationService $locationService){
+     $data =   $locationService->store($request->validated());
+
+   
+        $vehicle = Vehicle::where('user_id', auth('api')->id())->first();
+
+        if ($vehicle) {
+                event(new LocationUpdated($vehicle));
+            }
+       
+return $this->success(
+    new LocationResource($data),
+    'Operation successful',
+    201
+);
+     }
+>>>>>>> Stashed changes
 }
--- routes/channels.php
+++ routes/channels.php
@@ -1,6 +1,7 @@
 <?php
 
 use Illuminate\Support\Facades\Broadcast;
+use App\Models\User;
 
 use App\Models\User;
 
@@ -9,7 +10,16 @@ Broadcast::channel('App.Models.User.{id}', function ($user, $id) {
     return (int) $user->id === (int) $id;
 });
 
+<<<<<<< Updated upstream
 Broadcast::channel('vehicles', function (User $user) {
    
     return $user->role === 'manager';
 });
+=======
+
+Broadcast::channel('vehicles', function (User $user) {
+  
+    return !is_null($user);
+});
+
+>>>>>>> Stashed changes
