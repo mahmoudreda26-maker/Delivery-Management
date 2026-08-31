8a7a43061e1004f6e7fec159d9c84d9455149621
mahmoudreda26-maker <engmo1200@gmail.com>
2026-08-31 17:53:41 +0300

    Fix location broadcasting

--- app/Http/Controllers/Api/LocationController.php
+++ app/Http/Controllers/Api/LocationController.php
@@ -7,8 +7,6 @@ use App\Http\Controllers\Controller;
 use App\Http\Requests\LocationHistoryRequest;
 use App\Http\Requests\LocationRequest;
 use App\Http\Resources\LocationResource;
-use App\Models\Location;
-use App\Models\Vehicle;
 use App\Services\LocationService;
 use App\Traits\ApiResponse;
 
@@ -18,23 +16,23 @@ class LocationController extends Controller
 
     public function store(LocationRequest $request, LocationService $locationService)
     {
-        $data = $locationService->store($request->validated());
+        $location = $locationService->store(
+            $request->validated()
+        );
 
-        $location = Location::where('user_id', auth()->id())->first();
-
-        if ($location) {
-            event(new LocationUpdated($location));
-        }
+        event(new LocationUpdated($location));
 
         return $this->success(
-            new LocationResource($data),
+            new LocationResource($location),
             'Operation successful',
             201
         );
     }
 
-    public function history(LocationHistoryRequest $request, LocationService $locationService)
-    {
+    public function history(
+        LocationHistoryRequest $request,
+        LocationService $locationService
+    ) {
         $locations = $locationService->history(
             $request->validated()
         );
